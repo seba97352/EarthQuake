ba1ca0785148b68b6115f5770a12fa37aa648f03
Vojtěch Šebek <seba97352@macbook-air-sebek-vojtech.local>
2016-09-05 22:51:49 +0200

    Initial Commit

---
+++ EarthQuake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EarthQuake
+//
+//  Created by Vojtěch Šebek on 05.09.16.
+//  Copyright © 2016 Vojtěch Šebek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
